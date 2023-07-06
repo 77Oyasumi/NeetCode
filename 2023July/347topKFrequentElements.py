354ada2102d2851f45679621d6840cf4ea258c9b
77Oyasumi <77oyasumi@77OyasumideMacBook-Air.local>
2023-07-06 15:12:43 +0800

    347. top K Freq Elem

---
+++ 2023July/347topKFrequentElements.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq= [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n ,c in count.items():
+            freq[c].append(n)
+        
+        res = []
+
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+                
+solution = Solution()
+
+nums = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10]
+k = 3
+
+result = solution.topKFrequent(nums, k)
+print(result)
