eed5729d7b6f1bfc927ebcf0bd7c44e6eea285af
77Oyasumi <77oyasumi@77OyasumideMacBook-Air.local>
2023-07-20 19:05:11 +0800

    11. Water Container

---
+++ 2023July/11Container.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def conatinerWater(self, height:List[int])-> int:
+
+        res = 0
+
+        l, r = 0, len(height) - 1
+        while l < r:
+            area = (r - l) * min(height[l], height[r])
+            res = max(area, res)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+    
+solution = Solution()
+
+height1 = [1,8,6,2,5,4,8,3,7]
+print(solution.conatinerWater(height1))
