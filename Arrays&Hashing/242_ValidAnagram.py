4553c4552fb27446ae5fb22c10a45df5bef5a4a5
77Oyasumi <77oyasumi@77OyasumideMacBook-Air.local>
2023-07-28 10:28:54 +0800

    .get -> ()

--- Arrays&Hashing/242_ValidAnagram.py
+++ Arrays&Hashing/242_ValidAnagram.py
@@ -10,8 +10,8 @@ class Solution:
         countS, countT = {}, {}
 
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get[s[i], 0]
-            countT[t[i]] = 1 + countT.get[t[i], 0]
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
         for c in countS:
             if countS[c] != countT.get(c, 0):
                 return False
