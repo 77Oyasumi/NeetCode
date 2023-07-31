b794f96ad404949407c2cbab6ef400f289196732
77Oyasumi <77oyasumi@77OyasumideMacBook-Air.local>
2023-07-31 15:10:23 +0800

    solution using hashmap

--- Stacks/853_carFleet.py
+++ Stacks/853_carFleet.py
@@ -11,6 +11,16 @@ class Solution:
                 stack.pop()
         return len(stack)
     
+    def carfleet2(self, target: int, pos: List[int], speed: List[int]) -> int:
+        fleet_dict = {p:s for p, s in zip(pos, speed)}
+
+        stack = []
+        for p in sorted(fleet_dict.keys(), reverse=True):
+            stack.append((target - p) / fleet_dict[p])
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+    
 solution = Solution()
 
 
@@ -24,3 +34,12 @@ if __name__ == "__main__":
 
     target2 = 100; position2 = [0,2,4]; speed2 = [4,2,1]
     print(solution.carfleet(target2, position2, speed2))
+
+    target = 12; position = [10,8,0,5,3]; speed = [2,4,1,1,3]
+    print(solution.carfleet2(target, position, speed))
+
+    target1 = 10; position1 = [3]; speed1 = [3]
+    print(solution.carfleet2(target1, position1, speed1))
+
+    target2 = 100; position2 = [0,2,4]; speed2 = [4,2,1]
+    print(solution.carfleet2(target2, position2, speed2))
