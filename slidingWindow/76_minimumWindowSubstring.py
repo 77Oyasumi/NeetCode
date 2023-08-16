26822b2adceebc9dc4f3488d9810a85c6741421b
77Oyasumi <77oyasumi@77OyasumideMacBook-Air.local>
2023-08-16 17:40:57 +0800

    76 minimum window substring

---
+++ slidingWindow/76_minimumWindowSubstring.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "": return ""
+        countT, window = {}, {}
+
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+            if c in countT and window[c] == countT[c]:
+                have += 1
+            while have == need: # shrink
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = (r - l + 1)
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l:r+1] if resLen != float("infinity") else ""
+    
+if __name__ == "__main__":
+    
+    solution = Solution()
+
+    s = "ADOBECODEBANC"; t = "ABC"
+    print(f"{solution.minWindow(s,t)}")
+    s = "a"; t = "a"
+    print(f"{solution.minWindow(s,t)}")
+    s = "a"; t = "aa"
+    print(f"{solution.minWindow(s,t)}")
+    s = "qwdinadoijvapisnd"; t = "ijp"
+    print(f"{solution.minWindow(s,t)}")
