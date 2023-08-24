104e9430faa9eb4c8c196d35f2df9aab34cde38c
77Oyasumi <77oyasumi@77OyasumideMacBook-Air.local>
2023-08-24 10:18:34 +0800

    add example in 21

--- linkedList/21_mergeTwoSortedList.py
+++ linkedList/21_mergeTwoSortedList.py
@@ -24,3 +24,38 @@ class Solution:
         elif l2:
             tail.next = l2
         return dummy.next
+    
+if __name__ == "__main__":
+    # 建立 list1 和 list2
+    list1 = ListNode(1, ListNode(2, ListNode(4)))
+    list2 = ListNode(1, ListNode(3, ListNode(4)))
+
+    # 顯示 list1 原本的樣子
+    print("List 1 original:")
+    current = list1
+    while current:
+        print(f"{current.val} -> ", end="")
+        current = current.next
+    print("None\n")
+
+    # 顯示 list2 原本的樣子
+    print("List 2 original:")
+    current = list2
+    while current:
+        print(f"{current.val} -> ", end="")
+        current = current.next
+    print("None\n")
+
+    # 建立 Solution 物件
+    solution = Solution()
+
+    # 合併 list1 和 list2
+    merged_list = solution.mergeTwoLists(list1, list2)
+
+    # 顯示合併後的結果
+    print("Merged list:")
+    current = merged_list
+    while current:
+        print(f"{current.val} -> ", end="")
+        current = current.next
+    print("None\n")
