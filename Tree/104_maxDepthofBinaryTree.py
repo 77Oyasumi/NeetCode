e97ba55c480c26d13b585cecc607e97b566659f4
77Oyasumi <xihan1205@gmail.com>
2023-09-19 19:34:46 +0800

    104. Maximum Depth of Binary Tree

---
+++ Tree/104_maxDepthofBinaryTree.py
@@ -0,0 +1,68 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+'''
+        if not root:
+            return 0
+        
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+
+        return max(left_depth, right_depth) + 1
+'''
+'''
+        if not root:
+            return 0
+        level = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
+'''
+'''
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth+1])
+                stack.append([node.right, depth+1])
+        return res
+'''
+
+if __name__ == "__main__":
+    # Create a sample binary tree
+    #        3
+    #       / \
+    #      9   20
+    #         /  \
+    #        15   7
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+
+    solution = Solution()
+    
+    # Test the maxDepth function
+    print("Max depth of the binary tree:", solution.maxDepth(root))  # Expected output: 3
