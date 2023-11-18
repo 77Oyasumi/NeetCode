3a8d616ab8a21cda8b0c85636d938cb796606a37
77Oyasumi <xihan1205@gmail.com>
2023-11-18 18:14:43 +0800

    105. Construct Binary Tree from Preorder and Inorder

---
+++ Tree/105_constructBinaryTree.py
@@ -0,0 +1,29 @@
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f"TreeNode(val={self.val}, left={self.left}, right={self.right})"
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        return root
+
+if __name__ == "__main__":
+    sol = Solution()
+    preorder = [3,9,20,15,7]
+    inorder = [9,3,15,20,7]
+    tree_root = sol.buildTree(preorder, inorder)
+    print(tree_root)
